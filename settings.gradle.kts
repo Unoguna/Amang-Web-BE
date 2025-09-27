7a2cac9aa98a742f7493df48ba30053bd3ac3c19
Jeongho Kim <kimjungho.kr@gmail.com>
2025-09-27 13:41:41 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Amang_Web_BE"
---
+++ src/main/java/back/amang_web_be/AmangWebBeApplication.java
@@ -0,0 +1,13 @@
+package back.amang_web_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmangWebBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmangWebBeApplication.class, args);
+    }
+
+}
